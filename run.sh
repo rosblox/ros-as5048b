b2d2a558aea313f0989bfd3dcfe38035ca087417
Max Polzin <polzin.max@gmail.com>
2025-05-07 19:14:15 +0200

    Prob. not.

--- run.sh
+++ run.sh
@@ -2,25 +2,6 @@
 
 REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
 
-docker run -it --rm \
---network=host \
---ipc=host --pid=host \
---env UID=$(id -u) \
---env GID=$(id -g) \
---env I2C_GID=$(getent group i2c | cut -d: -f3) \
---device=/dev/i2c-1 \
---env I2C_DEVICE=1 \
---env I2C_ADDRESS=0x40 \
--v /dev:/dev  \
-ghcr.io/rosblox/${REPOSITORY_NAME}:humble
-
-
-
-
-#!/bin/bash
-
-REPOSITORY_NAME="$(basename "$(dirname -- "$( readlink -f -- "$0"; )")")"
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export HOST_UID=$(id -u)
