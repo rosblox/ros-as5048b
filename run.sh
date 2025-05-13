115a66b843ff99a925da217ec1f1aa45599144c0
Max Polzin <polzin.max@gmail.com>
2025-05-13 11:36:02 +0200

    Update

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export HOST_UID=$(id -u)
 
 docker compose -f $SCRIPT_DIR/docker-compose.yml run \
---volume ./ros_as5048b:/colcon_ws/src/ros_as5048b \
+--volume $(pwd)/ros_as5048b:/colcon_ws/src/ros_as5048b \
 ${REPOSITORY_NAME} bash
